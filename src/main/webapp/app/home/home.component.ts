5cdbc9c7b74bfe0578c1a280900a53254704907f
aimero <ezanaaimero@gmail.com>
2018-04-17 14:16:15 -0400

    ODOS_24: Added building images

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -15,6 +15,7 @@ import { Account, LoginModalService, Principal } from '../shared';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
+    buildings: any;
 
     constructor(
         private principal: Principal,
@@ -28,6 +29,63 @@ export class HomeComponent implements OnInit {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
+        this.buildings = [
+            {
+                "id": 1,
+                "building_name": "Building A",
+                "building_description": "building a: Lorem ipsum dolor sit amet, consectetur adipisicing elit. At autem harum in libero officiis, quos repellat velit! Alias animi dicta, dolorum, ipsum iure molestias obcaecati provident quaerat quo vitae voluptatem?\n",
+                "conference_rooms":[
+                    {
+                        "room_number": 43,
+                        "room_name": "conference room a"
+                    },
+                    {
+                        "room_number": 9,
+                        "room_name": "conference room b"
+                    },
+                    {
+                        "room_number": 33,
+                        "room_name": "conference room c"
+                    },
+                    {
+                        "room_number": 501,
+                        "room_name": "conference room b"
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "building_name": "Building B",
+                "building_description": "building b: Lorem ipsum dolor sit amet, consectetur adipisicing elit. At autem harum in libero officiis, quos repellat velit! Alias animi dicta, dolorum, ipsum iure molestias obcaecati provident quaerat quo vitae voluptatem?\n",
+                "conference_rooms":[
+                    {
+                        "room_number": 123,
+                        "room_name": "conference room 1"
+                    },
+                    {
+                        "room_number": 23,
+                        "room_name": "conference room 2"
+                    }
+                ]
+            },
+            {
+                "id": 3,
+                "building_name": "Building C ",
+                "building_description": "building c: Lorem ipsum dolor sit amet, consectetur adipisicing elit. At autem harum in libero officiis, quos repellat velit! Alias animi dicta, dolorum, ipsum iure molestias obcaecati provident quaerat quo vitae voluptatem?\n",
+                "conference_rooms":[
+                    {
+                        "room_number": 13,
+                        "room_name": "conference room a"
+                    },
+                    {
+                        "room_number": 3,
+                        "room_name": "conference room b"
+                    }
+                ]
+
+
+            }
+        ];
     }
 
     registerAuthenticationSuccess() {
