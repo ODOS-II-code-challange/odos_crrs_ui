f7f31064bbc374edb56b2e92e89c238219e64531
aimero <ezanaaimero@gmail.com>
2018-04-17 19:04:34 -0400

    ODOS_28: Created conference room component

---
+++ src/main/webapp/app/conference-room/conference-room.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { OdosCrrsUiSharedModule } from '../shared';
+import { CONFERENCE_ROOM_ROUTE, ConferenceRoomComponent } from "./";
+
+
+@NgModule({
+    imports: [
+        OdosCrrsUiSharedModule,
+        RouterModule.forChild([ CONFERENCE_ROOM_ROUTE ])
+    ],
+    declarations: [
+        ConferenceRoomComponent,
+    ],
+    entryComponents: [
+    ],
+    providers: [
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class OdosCrrsUiConferenceRoomModule {}
+
