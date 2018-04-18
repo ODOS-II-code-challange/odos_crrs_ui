b60f2e245f6ad82a1ea22435195e72a76e22f1e8
Ezana Aimero <ezanaaimero@gmail.com>
2018-04-18 19:32:13 -0400

    ODOS_31: added the formgroup, formControlName and create formgroup

--- src/main/webapp/app/reservation/reservation.component.ts
+++ src/main/webapp/app/reservation/reservation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder,
+  Validators,ReactiveFormsModule, FormControl  } from '@angular/forms';
 
 @Component({
   selector: 'jhi-reservation',
@@ -8,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ReservationComponent implements OnInit {
+  reservationform:FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  conferenceTitle: FormControl;
+  conferenceDescription: FormControl;
 
   constructor() { }
 
-  ngOnInit() { }
-
+  ngOnInit() { 
+    this.reservationform = new FormGroup({
+        firstName: new FormControl('', Validators.required),
+        lastName: new FormControl('', Validators.required),
+        email: new FormControl('', Validators.required),
+        conferenceTitle: new FormControl('', Validators.required),
+        conferenceDescription: new FormControl('', Validators.required)
+    });
+  }
 }
