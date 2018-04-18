357b1eafdb3fd7113d2ba1216df6b524cc592b26
aimero <ezanaaimero@gmail.com>
2018-04-18 17:43:25 -0400

    ODOS_33: created end point to reach out and get all building infos.

--- src/main/webapp/app/home/home.service.ts
+++ src/main/webapp/app/home/home.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { CRRS_API_URL } from '../app.constants';
+import { BuildingInfo } from './home.model';
 
 @Injectable()
 export class HomeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllBuildingData() : Observable<BuildingInfo[]>{
+      return this.http.get<BuildingInfo[]>( CRRS_API_URL + 'api/buildings');
+  }
 
 }
