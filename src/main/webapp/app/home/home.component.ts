edfb45292d113636486cbc0f4eae269f0daf7923
aimero <ezanaaimero@gmail.com>
2018-04-19 14:08:00 -0400

    ODOS_24: calls backend to retrieve all building info.

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 
 import { Account, LoginModalService, Principal } from '../shared';
+import { HomeService } from './home.service';
 
 @Component({
     selector: 'jhi-home',
@@ -20,7 +21,8 @@ export class HomeComponent implements OnInit {
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
+        private eventManager: JhiEventManager,
+        private homeService : HomeService
     ) {
     }
 
@@ -29,63 +31,15 @@ export class HomeComponent implements OnInit {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
-        this.buildings = [
-            {
-                "id": 1,
-                "building_name": "Building A",
-                "building_description": "building a: Lorem ipsum dolor sit amet, consectetur adipisicing elit. At autem harum in libero officiis, quos repellat velit! Alias animi dicta, dolorum, ipsum iure molestias obcaecati provident quaerat quo vitae voluptatem?\n",
-                "conference_rooms":[
-                    {
-                        "room_number": 43,
-                        "room_name": "conference room a"
-                    },
-                    {
-                        "room_number": 9,
-                        "room_name": "conference room b"
-                    },
-                    {
-                        "room_number": 33,
-                        "room_name": "conference room c"
-                    },
-                    {
-                        "room_number": 501,
-                        "room_name": "conference room b"
-                    }
-                ]
-            },
-            {
-                "id": 2,
-                "building_name": "Building B",
-                "building_description": "building b: Lorem ipsum dolor sit amet, consectetur adipisicing elit. At autem harum in libero officiis, quos repellat velit! Alias animi dicta, dolorum, ipsum iure molestias obcaecati provident quaerat quo vitae voluptatem?\n",
-                "conference_rooms":[
-                    {
-                        "room_number": 123,
-                        "room_name": "conference room 1"
-                    },
-                    {
-                        "room_number": 23,
-                        "room_name": "conference room 2"
-                    }
-                ]
-            },
-            {
-                "id": 3,
-                "building_name": "Building C ",
-                "building_description": "building c: Lorem ipsum dolor sit amet, consectetur adipisicing elit. At autem harum in libero officiis, quos repellat velit! Alias animi dicta, dolorum, ipsum iure molestias obcaecati provident quaerat quo vitae voluptatem?\n",
-                "conference_rooms":[
-                    {
-                        "room_number": 13,
-                        "room_name": "conference room a"
-                    },
-                    {
-                        "room_number": 3,
-                        "room_name": "conference room b"
-                    }
-                ]
 
-
+        this.homeService.getAllBuildingData().subscribe(
+            (response) => {
+                this.buildings = response;
+            },
+            (error) => {
+                console.log(error);
             }
-        ];
+        )
     }
 
     registerAuthenticationSuccess() {
