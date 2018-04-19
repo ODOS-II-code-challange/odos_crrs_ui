e982a83fea8f1c874a930fb34c12eee5c4adcba4
Ezana Aimero <ezanaaimero@gmail.com>
2018-04-19 14:34:30 -0400

    ODOS_31: added the post reservation schedule method to database

--- src/main/webapp/app/reservation/reservation.component.ts
+++ src/main/webapp/app/reservation/reservation.component.ts
@@ -1,78 +1,102 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router}  from '@angular/router';
 import { FormGroup, FormArray, FormBuilder,
   Validators,ReactiveFormsModule, FormControl  } from '@angular/forms';
+import { ReservationService } from './reservation.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { LOGIN_ALREADY_USED_TYPE, EMAIL_ALREADY_USED_TYPE } from '../shared/constants/error.constants';
+// import { LOGIN_ALREADY_USED_TYPE,EMAIL_ALREADY_USED_TYPE } from 'src/main/webapp/app/shared';
 
-@Component({
-  selector: 'jhi-reservation',
-  templateUrl: './reservation.component.html',
-  styleUrls: [
-      'reservation.scss'
-  ]
-})
-export class ReservationComponent implements OnInit {
-  reservationDetailForm:FormGroup;
-  firstName: FormControl;
-  lastName: FormControl;
-  email: FormControl;
-  conferenceTitle: FormControl;
-  conferenceDescription: FormControl;
 
-  reservationTimeForm:FormGroup;
-  startDate: FormControl;
-  startTime: FormControl;
-  endTime: FormControl;
+  @Component({
+    selector: 'jhi-reservation',
+    templateUrl: './reservation.component.html',
+    styleUrls: [
+        'reservation.scss'
+    ]
+  })
+  export class ReservationComponent implements OnInit {
+    reservationDetailForm:FormGroup;
+    firstName: FormControl;
+    lastName: FormControl;
+    email: FormControl;
+    conferenceTitle: FormControl;
+    conferenceDescription: FormControl;
 
-  isReservationDetailForm: boolean = true;
-  isReservationTimeForm: boolean = false;
-  isReservationCompleteForm: boolean = false;
+    roomName: string;
+    success: boolean;
+    error: string;
+    errorEmailExists: string;
+    errorUserExists: string;
 
-  constructor() { }
+    reservationTimeForm:FormGroup;
+    startDate: FormControl;
+    startTime: FormControl;
+    endTime: FormControl;
 
-  ngOnInit() { 
-    this.reservationDetailForm = new FormGroup({
-        firstName: new FormControl('', Validators.required),
-        lastName: new FormControl('', Validators.required),
-        email: new FormControl('', Validators.required),
-        conferenceTitle: new FormControl('', Validators.required),
-        conferenceDescription: new FormControl('', Validators.required)
-    });
+    isReservationDetailForm: boolean = true;
+    isReservationTimeForm: boolean = false;
+    isReservationCompleteForm: boolean = false;
 
-    this.reservationTimeForm = new FormGroup({
-        Date: new FormControl('',Validators.required),
-        startTime: new FormControl('', Validators.required),
-        endTime: new FormControl('', Validators.required)
-    });
+    constructor( private router: Router, private route: ActivatedRoute, private service: ReservationService) { }
 
-    
-  }
+    ngOnInit() { 
 
-  saveReservationDetails(){
+      this.route.params.subscribe((params: Params) => {
+        this.roomName = params['roomName'];
+      })
 
-    console.log("reservation detail : ", this.reservationDetailForm.getRawValue());
+      this.reservationDetailForm = new FormGroup({
+          firstName: new FormControl('', Validators.required),
+          lastName: new FormControl('', Validators.required),
+          email: new FormControl('', [Validators.email, Validators.required]),
+          conferenceTitle: new FormControl('', Validators.required),
+          conferenceDescription: new FormControl('', Validators.required)
+      });
 
-    this.isReservationDetailForm = false;
-    this.isReservationTimeForm = true;
-  }
-  saveReservationTime(){
-  
-  
-    // var x = this.reservationDetailForm.get('startTime') - this.reservationDetailForm.get('endTime');
-    console.log("reservation detail : ", this.reservationTimeForm.getRawValue());
-    let start = +this.reservationTimeForm.get('startTime').value;
-    let end = +this.reservationTimeForm.get('startTime').value;
-    let result =(end - start)*60;
-    console.log("Start time: " + start);
-    console.log("Start time: " + end);
-    console.log("reservation start time : ", +this.reservationTimeForm.get('startTime').value - this.reservationTimeForm.get('startTime').value);
+      this.reservationTimeForm = new FormGroup({
+          startDate: new FormControl('',Validators.required),
+          startTime: new FormControl('', Validators.required),
+          endTime: new FormControl('', Validators.required)
+      });
 
-    this.isReservationTimeForm = false;
-    this.isReservationCompleteForm = true;
-  }
-  sumbitReservation(){
+      
+    }
+    reservation_info = {
+      'requestorId':'',
+      'roomScheduleStartTime':'',
+      'roomScheduleEndTime':'',
+      'conferenceTitle':'',
+      'conferenceRoomId': this.roomName,
 
-  }
-  disabled(date, mode){
-    return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+    }
+
+    saveReservationDetails(){
+      this.reservation_info.requestorId = this.reservationDetailForm.get('email').value;
+      this.reservation_info.conferenceTitle = this.reservationDetailForm.get('conferenceTitle').value;
+
+      this.isReservationDetailForm = false;
+      this.isReservationTimeForm = true;
+    }
+    saveReservationTime(){
+      this.reservation_info.roomScheduleStartTime = this.reservationTimeForm.get('startTime').value;
+      this.reservation_info.roomScheduleEndTime = this.reservationTimeForm.get('endTime').value;
+      this.service.postReservationData(this.reservation_info)
+      .subscribe(()=>{
+      }, (response)=> this.processError(response))
+
+      this.isReservationTimeForm = false;
+      this.isReservationCompleteForm = true;
+    }
+    private processError(response: HttpErrorResponse) {
+      this.success = null;
+      if (response.status === 400 && response.error.type === LOGIN_ALREADY_USED_TYPE) {
+          this.errorUserExists = 'ERROR';
+      } else if (response.status === 400 && response.error.type === EMAIL_ALREADY_USED_TYPE) {
+          this.errorEmailExists = 'ERROR';
+      } else {
+          this.error = 'ERROR';
+    }
   }
 
 }
