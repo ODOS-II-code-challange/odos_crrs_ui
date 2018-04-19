e63f4e9e6fc5e7344c4f967dfc50a84c1b2095a3
Ezana Aimero <ezanaaimero@gmail.com>
2018-04-18 22:54:43 -0400

    ODOS_32: report page search bar addedon UI

--- src/main/webapp/app/report/report.component.ts
+++ src/main/webapp/app/report/report.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
+import {ChartModule} from 'primeng/chart';
+import {NgbDateStruct, NgbCalendar, NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
+// import { TabsModule, BsDropdownModule } from 'ngx-bootstrap';
 
 import { Account, LoginModalService, Principal } from '../shared';
 
@@ -12,8 +15,10 @@ import { Account, LoginModalService, Principal } from '../shared';
   ]
 })
 export class ReportComponent implements OnInit {
+    filteredOption: string;
     account: Account;
     modalRef: NgbModalRef;
+
 
     constructor(
         private principal: Principal,
@@ -36,12 +41,33 @@ export class ReportComponent implements OnInit {
             });
         });
     }
-
+    
     isAuthenticated() {
         return this.principal.isAuthenticated();
     }
 
     login() {
         this.modalRef = this.loginModalService.open();
+    }
+    Report_table_header={
+        "Building": 1,
+        "ConfRm": 1,
+        "User": 1
+    }
+    tittle = ["Conference room Name", "Reserver", "Time"];
+    filterByBld(){
+        this.filteredOption="building";
+        this.tittle = ["Conference room Name", "Reserver", "Time"];
+        
+    }
+    filterByRoom(){
+        this.filteredOption="Room";
+        this.tittle = ["Building Name", "Reserver", "Conference Tittle", "Time"];
+        
+    }
+    filterByUser(){
+        this.filteredOption="User";
+        this.tittle = ["Building Name", "Conference room Name", "Conference Tittle", "Time"];
+
     }
 }
