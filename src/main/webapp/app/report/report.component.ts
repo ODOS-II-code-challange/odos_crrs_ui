65eb8c9d08321768448b40a8b2494e3e9fa8bf02
Ezana Aimero <ezanaaimero@gmail.com>
2018-04-19 02:22:27 -0400

    ODOS_32: report page UI updated

--- src/main/webapp/app/report/report.component.ts
+++ src/main/webapp/app/report/report.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
-import {ChartModule} from 'primeng/chart';
-import {NgbDateStruct, NgbCalendar, NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
-// import { TabsModule, BsDropdownModule } from 'ngx-bootstrap';
+import { ChartModule } from 'primeng/chart';
+import { NgbDateStruct, NgbCalendar, NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 
 import { Account, LoginModalService, Principal } from '../shared';
 
 @Component({
-  selector: 'jhi-report',
-  templateUrl: './report.component.html',
-  styleUrls: [
-      'report.scss'
-  ]
+    selector: 'jhi-report',
+    templateUrl: './report.component.html',
+    styleUrls: [
+        'report.scss'
+    ]
 })
 export class ReportComponent implements OnInit {
+    data: any;
     filteredOption: string;
     account: Account;
     modalRef: NgbModalRef;
@@ -25,6 +25,23 @@ export class ReportComponent implements OnInit {
         private loginModalService: LoginModalService,
         private eventManager: JhiEventManager
     ) {
+        this.data = {
+            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+            datasets: [
+                {
+                    label: 'Number of Reservations',
+                    backgroundColor: '#42A5F5',
+                    borderColor: '#1E88E5',
+                    data: [5, 10, 15, 20, 25, 30]
+                },
+                {
+                    label: 'Number of Cancelations',
+                    backgroundColor: '#9CCC65',
+                    borderColor: '#7CB342',
+                    data: [1, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+                }
+            ]
+        }
     }
 
     ngOnInit() {
@@ -41,7 +58,7 @@ export class ReportComponent implements OnInit {
             });
         });
     }
-    
+
     isAuthenticated() {
         return this.principal.isAuthenticated();
     }
@@ -49,24 +66,29 @@ export class ReportComponent implements OnInit {
     login() {
         this.modalRef = this.loginModalService.open();
     }
-    Report_table_header={
+
+
+
+    Report_table_header = {
         "Building": 1,
         "ConfRm": 1,
         "User": 1
     }
-    tittle = ["Conference room Name", "Reserver", "Time"];
-    filterByBld(){
-        this.filteredOption="building";
+    tittle = ["Building Name", "Conference room Name", "Reserver", "Time"];
+
+    filterByBld() {
+        this.filteredOption = "building";
         this.tittle = ["Conference room Name", "Reserver", "Time"];
-        
+
     }
-    filterByRoom(){
-        this.filteredOption="Room";
+    filterByRoom() {
+        this.filteredOption = "Room";
         this.tittle = ["Building Name", "Reserver", "Conference Tittle", "Time"];
-        
+
+
     }
-    filterByUser(){
-        this.filteredOption="User";
+    filterByUser() {
+        this.filteredOption = "User";
         this.tittle = ["Building Name", "Conference room Name", "Conference Tittle", "Time"];
 
     }
