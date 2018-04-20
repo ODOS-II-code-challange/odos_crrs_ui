0f361b86d3524e30c2270b7b587c6b07c68e0981
aimero <ezanaaimero@gmail.com>
2018-04-20 14:39:46 -0400

    ODOS_28: check if user is logged in before making a reservation

--- src/main/webapp/app/conference-room/conference-room.component.ts
+++ src/main/webapp/app/conference-room/conference-room.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
 import { JhiEventManager } from "ng-jhipster";
-import { Router, ActivatedRoute, Params, Data } from '@angular/router';
+import {Router, ActivatedRoute, Params, Data, RouterStateSnapshot, ActivatedRouteSnapshot} from '@angular/router';
 
 import { Account, LoginModalService, Principal } from "../shared";
 import { ConferenceRoomService } from './conference-room.service';
+import {StateStorageService} from "../shared/auth/state-storage.service";
 
 @Component({
     selector: 'jhi-conference-room',
@@ -27,7 +28,8 @@ export class ConferenceRoomComponent implements OnInit {
         private eventManager: JhiEventManager,
         private route: ActivatedRoute,
         private conferenceRoomService: ConferenceRoomService,
-        private router: Router
+        private router: Router,
+        private stateStorageService: StateStorageService
     ) {
     }
 
@@ -42,6 +44,42 @@ export class ConferenceRoomComponent implements OnInit {
         this.registerAuthenticationSuccess();
     }
 
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Promise<boolean> {
+
+        const authorities = route.data['authorities'];
+        // We need to call the checkLogin / and so the principal.identity() function, to ensure,
+        // that the client has a principal too, if they already logged in by the server.
+        // This could happen on a page refresh.
+        return this.checkLogin(authorities, state.url);
+    }
+
+    checkLogin(authorities: string[], url: string): Promise<boolean> {
+        const principal = this.principal;
+        return Promise.resolve(principal.identity().then((account) => {
+
+            if (!authorities || authorities.length === 0) {
+                return true;
+            }
+
+            if (account) {
+                return principal.hasAnyAuthority(authorities).then((response) => {
+                    if (response) {
+                        return true;
+                    }
+                    return false;
+                });
+            }
+
+            this.stateStorageService.storeUrl(url);
+            this.router.navigate(['accessdenied']).then(() => {
+                // only show the login dialog, if the user hasn't logged in yet
+                if (!account) {
+                    this.loginModalService.open();
+                }
+            });
+            return false;
+        }));
+    }
     registerAuthenticationSuccess() {
         this.eventManager.subscribe('authenticationSuccess', (message) => {
             this.principal.identity().then((account) => {
