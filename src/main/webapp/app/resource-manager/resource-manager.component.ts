01252ffaa8a647c908233541df83733f707946e7
Ezana Aimero <ezanaaimero@gmail.com>
2018-04-20 02:50:48 -0400

    ODOS_47: added the resource selection option, and edit and view ablities for admin

--- src/main/webapp/app/resource-manager/resource-manager.component.ts
+++ src/main/webapp/app/resource-manager/resource-manager.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Principal } from '../shared';
 
 @Component({
   selector: 'jhi-resource-manager',
   templateUrl: './resource-manager.component.html',
-  styleUrls: []
+  styleUrls: [
+    'resource-manager.scss'
+  ]
 })
 export class ResourceManagerComponent implements OnInit {
-
+  
   constructor() { }
+  selectedResource:string;
 
+  resources = ["Building", "Conference Room", "Equipment"];
   ngOnInit() {
+    this.selectedResource = this.resources[0];
+    console.log("Principal user name"+Principal.name);
   }
 
 }
