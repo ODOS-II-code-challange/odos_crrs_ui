66d673586756ed74b085ab8e2fefa2f827bea937
aimero <ezanaaimero@gmail.com>
2018-04-20 11:02:06 -0400

    ODOS_32: modified page to show tabular data. still using hard coded data for graphical data

--- src/main/webapp/app/report/report.component.ts
+++ src/main/webapp/app/report/report.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
-import {ChartModule} from 'primeng/chart';
-import {NgbDateStruct, NgbCalendar, NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
-// import { TabsModule, BsDropdownModule } from 'ngx-bootstrap';
 
 import { Account, LoginModalService, Principal } from '../shared';
+import { ReportService } from "./report.service";
 
 @Component({
   selector: 'jhi-report',
@@ -15,16 +13,42 @@ import { Account, LoginModalService, Principal } from '../shared';
   ]
 })
 export class ReportComponent implements OnInit {
-    filteredOption: string;
     account: Account;
     modalRef: NgbModalRef;
-
+    filterBy: string;
+    filterResult: any;
+    filterOptions: string[] = ["Building Name", "Requester"];
+    reportColumn = ["Building Name", "Conference room Name", "Conference Title", "Reserver", "Time"];
+    filterMapping = {
+        "Building Name": "search/building/",
+        "Requester": "search/user/"
+    }
+    data: any;
+    selectedOption: string = this.filterOptions[0];
 
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
+        private eventManager: JhiEventManager,
+        private reportService: ReportService
     ) {
+        this.data = {
+            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+            datasets: [
+                {
+                    label: 'Number of Reservations',
+                    backgroundColor: '#42A5F5',
+                    borderColor: '#1E88E5',
+                    data: [5, 10, 15, 20, 25, 30]
+                },
+                {
+                    label: 'Number of Cancelations',
+                    backgroundColor: '#9CCC65',
+                    borderColor: '#7CB342',
+                    data: [1, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+                }
+            ]
+        }
     }
 
     ngOnInit() {
@@ -32,6 +56,7 @@ export class ReportComponent implements OnInit {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
+        // this.searchBy('search/building/' + );
     }
 
     registerAuthenticationSuccess() {
@@ -41,7 +66,7 @@ export class ReportComponent implements OnInit {
             });
         });
     }
-    
+
     isAuthenticated() {
         return this.principal.isAuthenticated();
     }
@@ -49,25 +74,18 @@ export class ReportComponent implements OnInit {
     login() {
         this.modalRef = this.loginModalService.open();
     }
-    Report_table_header={
-        "Building": 1,
-        "ConfRm": 1,
-        "User": 1
-    }
-    tittle = ["Conference room Name", "Reserver", "Time"];
-    filterByBld(){
-        this.filteredOption="building";
-        this.tittle = ["Conference room Name", "Reserver", "Time"];
-        
-    }
-    filterByRoom(){
-        this.filteredOption="Room";
-        this.tittle = ["Building Name", "Reserver", "Conference Tittle", "Time"];
-        
-    }
-    filterByUser(){
-        this.filteredOption="User";
-        this.tittle = ["Building Name", "Conference room Name", "Conference Tittle", "Time"];
 
+    searchBy(filterBy: string){
+
+        console.log("filter by", this.filterMapping[this.selectedOption], filterBy);
+        return this.reportService.getSearchResult(this.filterMapping[this.selectedOption] + filterBy).subscribe(
+            (response) => {
+                this.filterResult = response;
+            },
+            (error) => {
+                this.filterResult = []
+                console.log(error);
+            }
+        )
     }
 }
